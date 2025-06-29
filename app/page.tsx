570846c9631573fae5085b4ad366f673350ccf98
michaelshakra <m.shakra@emeatrade.de>
2025-06-29 16:11:38 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-emea-dark">
+        Willkommen bei michaelshakra.com
+      </h1>
+    </main>
+  );
+}
